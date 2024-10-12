c5d0806b68ba3748b042a5a83b1b524924279a55
ZH <147311198+nwdnys1@users.noreply.github.com>
2024-10-13 00:58:19 +0800

    UPDATE

---
+++ code/python/data.py
@@ -0,0 +1,57 @@
+class APP:
+    def __init__(self, APPId: str, APIKey: str, APISecret: str):
+        self.APPId = APPId
+        self.APIKey = APIKey
+        self.APISecret = APISecret
+
+
+# 创建 APP 实例并存储在列表中
+APP_list = [
+    APP(
+        APPId="21a88706",
+        APIKey="0833dd9079a62a37c815ab82c0fd96fa",
+        APISecret="NWI3ZTFiNzVlOWQ1ZDEyOTc3YjVmODkx",
+    ),
+    APP(
+        APPId="5fb8d78a",
+        APIKey="8dca7165cff031a03e634121a3469261",
+        APISecret="NTllNmU5YmJjZmEwZGUwNTdlYzg5Njg3",
+    ),
+    APP(
+        APPId="5112a02c",
+        APIKey="bef4768fcf5354adab0085747446c91f",
+        APISecret="ZmFlMzE4ZmMzNmZjZWVjMThlZjBjOTMw",
+    ),
+    APP(
+        APPId="99c5b6a4",
+        APIKey="dd0a0077ca5520d0fc21b0665092c526",
+        APISecret="MmYzNTEwOGFjMjA4ZWIyOGJlYTUzMmE1",
+    ),
+]
+
+
+# 请求数据
+def request_data(image_path, APPId):
+    return {
+        "header": {"app_id": APPId, "status": 3},
+        "parameter": {
+            "image_recognize": {
+                "output_text_result": {
+                    "encoding": "utf8",
+                    "compress": "raw",
+                    "format": "plain",
+                }
+            }
+        },
+        "payload": {"image": {"encoding": "png", "image": image_path, "status": 3}},
+    }
+
+
+# 请求地址
+request_url = "https://api.xf-yun.com/v1/private/sc45f0684"
+
+# 用于快速定位响应值
+
+response_path_list = [
+    "$..payload.output_text_result",
+]
---
+++ code/python/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+from sample.aipass_client import execute
+from data import *
+import os
+import json
+
+if __name__ == "__main__":
+
+    response_list = []
+    os.remove("data.json") if os.path.exists("data.json") else None
+
+    def ocr(path, data_set, cnt=0):
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in sorted(filenames):
+                file_path = os.path.join(dirpath, filename)
+                response = execute(
+                    request_url,
+                    request_data(file_path, APP_list[cnt % len(APP_list)].APPId),
+                    "POST",
+                    APP_list[cnt % len(APP_list)].APPId,
+                    APP_list[cnt % len(APP_list)].APIKey,
+                    APP_list[cnt % len(APP_list)].APISecret,
+                    response_list,
+                )
+                response["file_name"] = filename
+                response["data_set"] = data_set  # 用于区分数据集
+                response_list.append(response)
+                cnt += 1  # 用于循环使用APP_list中的不同KEY
+
+    ocr("input/1", 1)
+    ocr("input/2", 2)
+
+    with open("data.json", "a") as json_file:
+        json.dump(response_list, json_file, ensure_ascii=False)
